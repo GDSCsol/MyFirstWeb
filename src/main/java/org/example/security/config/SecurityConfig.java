d9f6795adb315c7947042ea9f601d0a541ae5d2f
SolfE <love23869318@gmail.com>
2024-02-05 21:22:01 +0900

    fix: fix h2 connect error

--- src/main/java/org/example/security/config/SecurityConfig.java
+++ src/main/java/org/example/security/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package org.example.security.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,11 +21,20 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((registry) ->
-                        registry.requestMatchers("/api/hello").permitAll()
+                        registry.requestMatchers("/api/hello")
+                                .permitAll()
                                 .anyRequest().authenticated()
                 )
                 .build();
     }
 
-    
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled",havingValue = "true")
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console());
+    }
+
+
+
 }
