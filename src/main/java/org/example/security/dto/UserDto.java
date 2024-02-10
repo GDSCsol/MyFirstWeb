0dd0385c139bf911626a3a6b39ebf0ab66112bca
SolfE <love23869318@gmail.com>
2024-02-10 21:05:36 +0900

    feat: add UserDto

---
+++ src/main/java/org/example/security/dto/UserDto.java
@@ -0,0 +1,45 @@
+package org.example.security.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.example.security.entity.User;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String nickname;
+
+    private Set<AuthorityDto> authorityDtoSet;
+
+    public static UserDto from(User user) {
+        if(user == null) return null;
+
+        return UserDto.builder()
+                .username(user.getName())
+                .nickname(user.getNickname())
+                .authorityDtoSet(user.getAuthorities().stream()
+                        .map(authority -> AuthorityDto.builder().authorityName(authority.getAuthority().getName()).build()) // TODO: 2024-02-10 수정 필요가능성 있음 
+                        .collect(Collectors.toSet()))
+                .build();
+    }
+}
