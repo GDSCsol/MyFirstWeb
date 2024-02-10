e39eb2210207c8f9440b1fee7ac479b13081b92f
SolfE <love23869318@gmail.com>
2024-02-10 21:01:33 +0900

    feat: add ErrorDto

---
+++ src/main/java/org/example/security/config/AuthorityDto.java
@@ -0,0 +1,12 @@
+package org.example.security.config;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDto {
+    private String authorityName;
+}
