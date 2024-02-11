9fa2428e1ddfa3c94f817dc54478d767e7e04196
SolfE <love23869318@gmail.com>
2024-02-11 14:25:23 +0900

    feat: add UserService

---
+++ src/main/java/org/example/security/Service/UserService.java
@@ -0,0 +1,57 @@
+package org.example.security.Service;
+
+import io.jsonwebtoken.lang.Collections;
+import org.example.security.dto.UserDto;
+import org.example.security.entity.Authority;
+import org.example.security.entity.User;
+import org.example.security.repository.UserRepository;
+import org.example.security.util.SecurityUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public UserDto signup(UserDto userDto) {
+        if (userRepository.findOneWithAuthoritiesByName(userDto.getUsername()).orElse(null) != null) {
+            throw new DuplicateMemberException("이미 가입되어 있는 유저입니다.");
+        }
+
+        Authority authority = Authority.builder()
+                // Todo 유저 권한 설정 ('ROLE_USER')
+                .build();
+
+        User user = User.builder()
+                .name(userDto.getUsername())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+        return UserDto.from(userRepository.save(user));
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto getUserWithAuthorities(String username) {
+        return UserDto.from(userRepository.findOneWithAuthoritiesByName(username).orElse(null));
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto getMyUserWithAuthorities() {
+        return UserDto.from(
+                SecurityUtil.getCurrentUsername()
+                        .flatMap(userRepository::findOneWithAuthoritiesByName)
+                        .orElseThrow(() -> new NotFoundMemberException("Member not found"))
+        );
+    }
+}
