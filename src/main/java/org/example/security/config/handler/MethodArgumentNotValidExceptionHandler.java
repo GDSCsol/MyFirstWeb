63520f3d27feedcd892e06016949988a6a491701
SolfE <love23869318@gmail.com>
2024-02-11 14:56:40 +0900

    feat: add ExceptionHandler

---
+++ src/main/java/org/example/security/config/handler/MethodArgumentNotValidExceptionHandler.java
@@ -0,0 +1,37 @@
+package org.example.security.config.handler;
+
+import org.example.security.dto.ErrorDto;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class MethodArgumentNotValidExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorDto methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        return processFieldErrors(fieldErrors);
+    }
+
+    private ErrorDto processFieldErrors(List<org.springframework.validation.FieldError> fieldErrors) {
+        ErrorDto errorDTO = new ErrorDto(HttpStatus.BAD_REQUEST.value(), "@Valid Error");
+        for (org.springframework.validation.FieldError fieldError: fieldErrors) {
+            errorDTO.addFieldError(fieldError.getObjectName(), fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return errorDTO;
+    }
+}
